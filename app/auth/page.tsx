e8297e19571bb6ee0845bf3d3cfdb04c255a7128
Chensokheng <chensokheng6@gmail.com>
2024-01-28 14:56:23 +0900

    feat: authentication

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,10 +1,22 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { KeyRound } from "lucide-react";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { supabaseBrowser } from "@/lib/supabase/browser";
 
 export default function page() {
+	const handleLoginWithOAuth = (provider: "github" | "google") => {
+		const supabase = supabaseBrowser();
+		supabase.auth.signInWithOAuth({
+			provider,
+			options: {
+				redirectTo: location.origin + "/auth/callback",
+			},
+		});
+	};
+
 	return (
 		<div className="flex items-center justify-center w-full h-screen">
 			<div className=" w-96 rounded-md border p-5 space-y-5 relative bg-slate-900">
@@ -20,12 +32,14 @@ export default function page() {
 					<Button
 						className=" w-full flex items-center gap-2 "
 						variant="outline"
+						onClick={() => handleLoginWithOAuth("github")}
 					>
 						<FaGithub /> Github
 					</Button>
 					<Button
 						className=" w-full flex items-center gap-2 "
 						variant="outline"
+						onClick={() => handleLoginWithOAuth("google")}
 					>
 						<FcGoogle /> Google
 					</Button>
---
+++ lib/supabase/server.ts
@@ -0,0 +1,18 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export function supabaseServer() {
+	const cookieStore = cookies();
+
+	return createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			cookies: {
+				get(name: string) {
+					return cookieStore.get(name)?.value;
+				},
+			},
+		}
+	);
+}
