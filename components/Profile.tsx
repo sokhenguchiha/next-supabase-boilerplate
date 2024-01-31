bd6632f3b0d90254d67d36e6acff898df1f25593
sokhenguchiha <sokhengchen@gmail.com>
2024-01-31 14:43:17 +0900

    feat: profile drop down

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -9,6 +9,15 @@ import { useQueryClient } from "@tanstack/react-query";
 import { usePathname, useRouter } from "next/navigation";
 import { protectedPaths } from "@/lib/constant";
 
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function Profile() {
 	const { isFetching, data } = useUser();
 	const queryClient = useQueryClient();
@@ -37,25 +46,33 @@ export default function Profile() {
 					<Button variant="outline">SignIn</Button>
 				</Link>
 			) : (
-				<>
-					{data?.image_url ? (
-						<Image
-							src={data.image_url || ""}
-							alt={data.display_name || ""}
-							width={50}
-							height={50}
-							className=" rounded-full  animate-fade ring-2 cursor-pointer"
-							onClick={handleLogout}
-						/>
-					) : (
-						<div
-							className="h-[50px] w-[50px] flex items-center justify-center ring-2 rounded-full text-2xl font-bold cursor-pointer"
-							onClick={handleLogout}
-						>
-							<h1>{data.email[0]}</h1>
-						</div>
-					)}
-				</>
+				<DropdownMenu>
+					<DropdownMenuTrigger>
+						<>
+							{data?.image_url ? (
+								<Image
+									src={data.image_url || ""}
+									alt={data.display_name || ""}
+									width={50}
+									height={50}
+									className=" rounded-full  animate-fade ring-2 cursor-pointer"
+								/>
+							) : (
+								<div className="h-[50px] w-[50px] flex items-center justify-center ring-2 rounded-full text-2xl font-bold cursor-pointer">
+									<h1>{data.email[0]}</h1>
+								</div>
+							)}
+						</>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem onClick={handleLogout}>
+							Logout
+						</DropdownMenuItem>
+						<DropdownMenuItem>Billing</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
 			)}
 		</div>
 	);
